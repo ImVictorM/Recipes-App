8af8ebffce0e13c867818898079706afaa742987
Victor Mendes <victorutdsim7@gmail.com>
2024-05-19 16:53:24 -0300

    refactor: drinks and meals page to use layout

--- src/pages/Drinks.tsx
+++ src/pages/Drinks.tsx
@@ -1,12 +1,40 @@
-import { Header, Footer } from "@/components";
+import { SearchBar } from "@/components";
+import { useAppSelector } from "@/hooks";
+import { BasicLayout } from "@/layouts";
+import { selectMenu } from "@/store/slices/menuSlice";
+import { selectVisibility } from "@/store/slices/visibilitySlice";
+import { Link } from "react-router-dom";
 
 function Drinks() {
-  return (
-    <div>
-      <Header title="Drinks" />
+  const visibility = useAppSelector(selectVisibility);
+  const menu = useAppSelector(selectMenu);
 
-      <Footer />
-    </div>
+  return (
+    <BasicLayout containHeaderSearchBar>
+      <h1 className="mt-4" data-testid="page-title">
+        Drinks
+      </h1>
+
+      {visibility.showSearchBar && <SearchBar searchFor="drinks" />}
+
+      <ul className="mt-5">
+        {menu.drinks.map(({ strDrink, strDrinkThumb, idDrink }, index) => {
+          return (
+            <Link key={idDrink} to={`/drinks/${idDrink}`}>
+              <li data-testid={`${index}-recipe-card`}>
+                <img
+                  src={strDrinkThumb}
+                  alt={strDrink}
+                  data-testid={`${index}-card-img`}
+                  className="img"
+                />
+                <p data-testid={`${index}-card-name`}>{strDrink}</p>
+              </li>
+            </Link>
+          );
+        })}
+      </ul>
+    </BasicLayout>
   );
 }
 
--- src/pages/Meals.tsx
+++ src/pages/Meals.tsx
@@ -1,11 +1,39 @@
-import { Footer, Header } from "@/components";
+import { SearchBar } from "@/components";
+import { useAppSelector } from "@/hooks";
+import { BasicLayout } from "@/layouts";
+import { selectMenu } from "@/store/slices/menuSlice";
+import { selectVisibility } from "@/store/slices/visibilitySlice";
+import { Link } from "react-router-dom";
 
 export default function Meals() {
-  return (
-    <div>
-      <Header title="Meals" />
+  const visibility = useAppSelector(selectVisibility);
+  const menu = useAppSelector(selectMenu);
 
-      <Footer />
-    </div>
+  return (
+    <BasicLayout containHeaderSearchBar>
+      <h1 className="mt-4" data-testid="page-title">
+        Meals
+      </h1>
+
+      {visibility.showSearchBar && <SearchBar searchFor="meals" />}
+
+      <ul className="mt-5">
+        {menu.meals.map(({ strMeal, strMealThumb, idMeal }, index) => {
+          return (
+            <Link key={idMeal} to={`/meals/${idMeal}`}>
+              <li data-testid={`${index}-recipe-card`}>
+                <img
+                  className="img"
+                  src={strMealThumb}
+                  alt={strMeal}
+                  data-testid={`${index}-card-img`}
+                />
+                <p data-testid={`${index}-card-name`}>{strMeal}</p>
+              </li>
+            </Link>
+          );
+        })}
+      </ul>
+    </BasicLayout>
   );
 }

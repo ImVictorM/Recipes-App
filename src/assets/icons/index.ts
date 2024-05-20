cf1d6942992285391c1a7b15b5283428f0bdd041
Victor Mendes <victorutdsim7@gmail.com>
2024-05-20 16:19:49 -0300

    style: change header style

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,6 +1,5 @@
 export { default as blackHeartIcon } from "./blackHeartIcon.svg";
-export { default as drinkIcon } from "./drinkIcon.svg";
-export { default as glassIcon } from "./glassIcon.svg";
+export { default as cocktailIcon } from "./cocktailIcon.svg";
 export { default as mealIcon } from "./mealIcon.svg";
 export { default as profileIcon } from "./profileIcon.svg";
 export { default as searchIcon } from "./searchIcon.svg";
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 import { profileIcon, searchIcon } from "@/assets/icons";
 import { headerLogo } from "@/assets/images";
-import "../styles/components/Header.css";
 import { useAppDispatch } from "@/hooks";
 import { toggleSearchBarVisibility } from "@/store/slices/visibilitySlice";
+import "@/sass/components/_header.scss";
 
 export type HeaderProps = {
   containSearchBar?: boolean;
@@ -18,27 +18,31 @@ export default function Header({ containSearchBar }: HeaderProps) {
 
   return (
     <>
-      <header className="header">
-        <img src={headerLogo} alt="recipes app logo" />
+      <header className="header d-flex align-items-center justify-content-center w-100">
+        <div className="header-content d-flex align-items-center justify-content-between">
+          <img src={headerLogo} alt="recipes app logo" />
 
-        {containSearchBar && (
-          <button
-            data-testid="search-top-btn"
-            className="icon-search"
-            type="button"
-            onClick={handleSearchBarVisibility}
-          >
-            <img src={searchIcon} alt="search magnifying glass" />
-          </button>
-        )}
+          <div className="d-flex gap-4">
+            {containSearchBar && (
+              <button
+                data-testid="search-top-btn"
+                type="button"
+                onClick={handleSearchBarVisibility}
+                className="header-search-button"
+              >
+                <img src={searchIcon} alt="search magnifying glass" />
+              </button>
+            )}
 
-        <Link to="/profile">
-          <img
-            src={profileIcon}
-            alt="user profile"
-            data-testid="profile-top-btn"
-          />
-        </Link>
+            <Link to="/profile">
+              <img
+                src={profileIcon}
+                alt="user profile"
+                data-testid="profile-top-btn"
+              />
+            </Link>
+          </div>
+        </div>
       </header>
     </>
   );

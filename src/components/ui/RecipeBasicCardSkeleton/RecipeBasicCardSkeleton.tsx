ad473346bd3f3a77e11bea24fffcb65b53807fe8
Victor Mendes <victorutdsim7@gmail.com>
2024-06-17 16:30:47 -0300

    test: RecipeBasicCardSkeleton component

--- src/components/ui/RecipeBasicCardSkeleton/RecipeBasicCardSkeleton.tsx
+++ src/components/ui/RecipeBasicCardSkeleton/RecipeBasicCardSkeleton.tsx
@@ -7,15 +7,26 @@ import styles from "@/sass/components/ui/RecipeBasicCardSkeleton.module.scss";
 
 export default function RecipeBasicCardSkeleton() {
   return (
-    <Card className={`${styles.card} shadow m-0`}>
+    <Card
+      className={`${styles.card} shadow m-0`}
+      data-testid="BasicCardSkeleton"
+    >
       <PlaceholderImage
         role="img"
         aria-label="placeholder"
         className="card-img-top"
+        data-testid="BasicCardSkeleton.ImgPlaceholder"
       />
 
-      <Card.Body className={`${styles.card__body}`}>
-        <Placeholder as={Card.Title} animation="glow">
+      <Card.Body
+        className={`${styles.card__body}`}
+        data-testid="BasicCardSkeleton.Body"
+      >
+        <Placeholder
+          as={Card.Title}
+          animation="glow"
+          data-testid="BasicCardSkeleton.Body.TitlePlaceholder"
+        >
           <Placeholder xs={getRandomBetweenInclusive(4, 11)} bg="slate" />
         </Placeholder>
       </Card.Body>
---
+++ tests/components/ui/RecipeBasicCardSkeleton.test.tsx
@@ -0,0 +1,18 @@
+import { within } from "@testing-library/dom";
+
+import RecipeBasicCardSkeleton from "@/components/ui/RecipeBasicCardSkeleton";
+
+import renderElement from "../../utils/render/renderElement";
+
+const RecipeBasicCardSkeletonDefault = () => <RecipeBasicCardSkeleton />;
+
+describe("component: RecipeBasicCardSkeleton", () => {
+  it("renders correctly", () => {
+    const { container } = renderElement(<RecipeBasicCardSkeletonDefault />);
+
+    within(container).getByTestId("BasicCardSkeleton");
+    within(container).getByTestId("BasicCardSkeleton.ImgPlaceholder");
+    within(container).getByTestId("BasicCardSkeleton.Body");
+    within(container).getByTestId("BasicCardSkeleton.Body.TitlePlaceholder");
+  });
+});
